baa6a03d9f9fd5f9c9fd33dedc06750dbcc3ff0f
Marc <marc.montecalvo@gmail.com>
2026-07-18 11:00:52 -0400

    Fix Windows networking tools module

--- modules/windows/networking.ps1
+++ modules/windows/networking.ps1
@@ -1,4 +1,3 @@
 $ErrorActionPreference='Stop'
 winget install --id Insecure.Nmap --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id arendst.TasmotaDeviceManager --exact --silent --accept-package-agreements --accept-source-agreements 2>$null
 Write-Host 'Windows already includes Test-NetConnection, Resolve-DnsName, Get-NetTCPConnection, tracert, pathping, netstat, and pktmon. Nmap was added.'
