39ac4729750920de44cfd331af4ba811d262b63a
Marc <marc.montecalvo@gmail.com>
2026-07-19 13:33:44 -0400

    Make Windows Starship configuration idempotent

--- modules/windows/starship.ps1
+++ modules/windows/starship.ps1
@@ -1,6 +1,3 @@
 $ErrorActionPreference='Stop'
 winget install --id Starship.Starship --exact --silent --accept-package-agreements --accept-source-agreements
-$ProfileDir=Split-Path -Parent $PROFILE
-New-Item -ItemType Directory -Force -Path $ProfileDir | Out-Null
-if (-not (Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE | Out-Null }
-if (-not (Select-String -Path $PROFILE -SimpleMatch 'starship init powershell' -Quiet)) { Add-Content $PROFILE "`n# machine-setup: starship`nInvoke-Expression (&starship init powershell)" }
+Set-ManagedBlock -Path $PROFILE -Name 'starship' -Content 'Invoke-Expression (&starship init powershell)'
