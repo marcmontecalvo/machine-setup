1732a093e4b38a897235a0582b085b4cf649727e
Marc <marc.montecalvo@gmail.com>
2026-07-19 13:31:46 -0400

    Make Starship configuration idempotent

--- modules/unix/starship.sh
+++ modules/unix/starship.sh
@@ -4,8 +4,6 @@ setup_starship() {
     if command -v brew >/dev/null; then brew install starship
     else curl -sS https://starship.rs/install.sh | sh -s -- -y; fi
   fi
-  for rc in "$HOME/.bashrc" "$HOME/.zshrc"; do
-    touch "$rc"
-    grep -q 'starship init' "$rc" || printf '\n# machine-setup: starship\neval "$(starship init %s)"\n' "$(basename "$rc" | sed 's/^\.//;s/rc$//')" >> "$rc"
-  done
+  replace_managed_block "$HOME/.bashrc" "starship" 'eval "$(starship init bash)"'
+  replace_managed_block "$HOME/.zshrc" "starship" 'eval "$(starship init zsh)"'
 }
