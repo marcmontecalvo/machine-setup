1abe41d11a2afbc55aa869f5780307ed87dd2ba1
Marc <marc.montecalvo@gmail.com>
2026-07-19 13:33:52 -0400

    Make Windows eza configuration idempotent

--- modules/windows/eza.ps1
+++ modules/windows/eza.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference='Stop'
 winget install --id eza-community.eza --exact --silent --accept-package-agreements --accept-source-agreements
-$ProfileDir=Split-Path -Parent $PROFILE
-New-Item -ItemType Directory -Force -Path $ProfileDir | Out-Null
-if (-not (Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE | Out-Null }
-if (-not (Select-String -Path $PROFILE -SimpleMatch 'function ll { eza' -Quiet)) { Add-Content $PROFILE "`n# machine-setup: eza`nfunction ll { eza -lah --group-directories-first --git @args }`nfunction tree { eza --tree @args }" }
+$Block = @'
+function ll { eza -lah --group-directories-first --git @args }
+function tree { eza --tree @args }
+'@
+Set-ManagedBlock -Path $PROFILE -Name 'eza' -Content $Block.TrimEnd()
